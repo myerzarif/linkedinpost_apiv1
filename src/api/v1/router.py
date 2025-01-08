b301f49e885b9d46584a027bc6209a68f156c783
mahyar <mahyarzarifkar@gmail.com>
2025-01-08 15:11:53 +0100

    add retrieve to routes

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -3,9 +3,12 @@ from api.v1.endpoints import auth
 from api.v1.endpoints import post
 from api.v1.endpoints import scrape
 from api.v1.endpoints import image
+from api.v1.endpoints import retrieve
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(post.router, prefix="/post", tags=["post"])
 api_router.include_router(scrape.router, prefix="/scrape", tags=["scrape"])
 api_router.include_router(image.router, prefix="/image", tags=["image"])
+api_router.include_router(
+    retrieve.router, prefix="/retrieve", tags=["retrieve"])
